98dd035d1f30373de443bf5c6b86f2c54e50870b
nogueirawadson <nogueirawadson@outlook.pt>
2024-09-18 14:48:56 -0300

    ajustes

--- inicio/assets/js/api.js
+++ inicio/assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/nogueirawadson/Portfolio/master/data/profile.json?token=GHSAT0AAAAAACROJSH2S327DU3XCWOP7OY4ZXKHRGQ'
+  const url = 'https://raw.githubusercontent.com/nogueirawadson/Portfolio/master/data/profile.json?token=GHSAT0AAAAAACROJSH2SBRCVTLNMVHPRB6EZXLBGWA'
   const fetching = await fetch(url)
   return await fetching.json()
 
