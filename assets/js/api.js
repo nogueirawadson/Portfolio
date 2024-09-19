53fac4953295cec4f9b5af13e81162a5da5d9d2a
nogueirawadson <nogueirawadson@outlook.pt>
2024-09-18 22:04:41 -0300

    Finalizar

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/nogueirawadson/js-developer-portfolio/refs/heads/main/data/profile.json'
+  const url = 'https://raw.githubusercontent.com/nogueirawadson/Portfolio/master/data/profile.json'
   const fetching = await fetch(url)
   return await fetching.json()
 
